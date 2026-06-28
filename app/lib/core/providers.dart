316b4bce4bc6e234beb7561a197606dede39be75
yurne21-arch <yurne.21@gmail.com>
2026-06-28 19:41:00 -0400

    Update providers.dart

--- app/lib/core/providers.dart
+++ app/lib/core/providers.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'data/local/app_database.dart';
 import 'data/local/local_cache_service.dart';
 import 'data/remote/supabase_service.dart';
 import 'data/sync/outbox_sync.dart';
@@ -10,24 +9,14 @@ import '../features/auth/domain/auth_repository.dart';
 import '../features/profile/data/profile_repository_impl.dart';
 import '../features/profile/domain/profile_repository.dart';
 
-/// Infraestructura
 final supabaseServiceProvider =
     Provider<SupabaseService>((ref) => const SupabaseService());
 
-final appDatabaseProvider = Provider<AppDatabase?>((ref) {
-  final db = openLocalDatabaseOrNull();
-  if (db != null) ref.onDispose(db.close);
-  return db;
-});
+final localCacheProvider =
+    Provider<LocalCacheService>((ref) => const LocalCacheService());
 
-final localCacheProvider = Provider<LocalCacheService>(
-  (ref) => LocalCacheService(ref.read(appDatabaseProvider)),
-);
+final syncPortProvider = Provider<SyncPort>((ref) => const OutboxSync());
 
-final syncPortProvider =
-    Provider<SyncPort>((ref) => OutboxSync(ref.read(appDatabaseProvider)));
-
-/// Repositorios
 final authRepositoryProvider = Provider<AuthRepository>(
   (ref) => AuthRepositoryImpl(ref.read(supabaseServiceProvider)),
 );
