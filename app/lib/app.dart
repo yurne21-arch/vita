33e0691d5a550d66d6af4a9eb0995c5d833602bb
yurne21-arch <yurne.21@gmail.com>
2026-06-28 19:14:05 -0400

    Update app.dart

--- app/lib/app.dart
+++ app/lib/app.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:go_router/go_router.dart';
 
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
-import 'l10n/app_localizations.dart';
 
 class VitaApp extends StatefulWidget {
   const VitaApp({super.key});
@@ -19,18 +17,11 @@ class _VitaAppState extends State<VitaApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      onGenerateTitle: (context) => AppLocalizations.of(context).appTitle,
+      title: 'VITA',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light(),
       darkTheme: AppTheme.dark(),
       themeMode: ThemeMode.system,
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: AppLocalizations.supportedLocales,
       routerConfig: _router,
     );
   }
